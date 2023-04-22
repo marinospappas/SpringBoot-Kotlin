bb1bf4ace3cbbb13284eb4355ce06c167ede9cf9
marinos <marinosch.99981@gmail.com>
2023-04-22 17:53:16 +0200

    EventForwarderApplication updated logger for main class

--- src/main/kotlin/mpdev/springboot/eventforwarder/EventForwarderApplication.kt
+++ src/main/kotlin/mpdev/springboot/eventforwarder/EventForwarderApplication.kt
@@ -1,14 +1,19 @@
 package mpdev.springboot.eventforwarder
 
-import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-@Slf4j
-class EventForwarderApplication
+class EventForwarderApplication {
+    init {
+        val log: Logger = LoggerFactory.getLogger(this::class.java)
+        log.info("Created EventForwarderApplication class")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<EventForwarderApplication>(*args)
